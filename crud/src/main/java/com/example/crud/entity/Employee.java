fc2bf48d212de6da4c73a3f66b8b634ef8867258
smony19 <115245614+smony19@users.noreply.github.com>
2023-07-16 14:13:58 +0100

    employee crud app using sprint boot

--- crud/src/main/java/com/example/crud/entity/Employee.java
+++ crud/src/main/java/com/example/crud/entity/Employee.java
@@ -1,2 +1,62 @@
-package com.example.crud.entity;public class Employee {
+package com.example.crud.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long employeeId;
+
+    private String employeeName;
+
+    private float employeeSalary;
+
+    private String employeeRole;
+
+    public Employee() {
+
+    }
+
+    public Employee(Long employeeId, String employeeName, float employeeSalary ){
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.employeeSalary = employeeSalary;
+    }
+
+    public Long getEmployeeId(){
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId){
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public float getEmployeeSalary() {
+        return employeeSalary;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public void setEmployeeSalary(float employeeSalary) {
+        this.employeeSalary = employeeSalary;
+    }
+
+    public String getEmployeeRole() {
+        return employeeRole;
+    }
+
+    public void setEmployeeRole(String employeeRole) {
+        this.employeeRole = employeeRole;
+    }
 }
--- crud/src/main/java/com/example/crud/repository/EmployeeRepository.java
+++ crud/src/main/java/com/example/crud/repository/EmployeeRepository.java
@@ -1,2 +1,10 @@
-package com.example.crud.repository;public interface EmployeeRepository {
+package com.example.crud.repository;
+
+import com.example.crud.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
 }
